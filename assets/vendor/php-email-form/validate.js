569372472915bb954b973f3e1de4999e1076c45d
JSD1982 <cardoso@josesegundo.digital>
2024-01-05 19:22:50 -0300

    valid email

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -27,29 +27,29 @@
 
       let formData = new FormData(thisForm);
 
-      if (recaptcha) {
-        if (typeof grecaptcha !== "undefined") {
-          grecaptcha.ready(function () {
-            try {
-              grecaptcha
-                .execute(recaptcha, { action: "php_email_form_submit" })
-                .then((token) => {
-                  formData.set("recaptcha-response", token);
-                  php_email_form_submit(thisForm, action, formData);
-                });
-            } catch (error) {
-              displayError(thisForm, error);
-            }
-          });
-        } else {
-          displayError(
-            thisForm,
-            "The reCaptcha javascript API url is not loaded!"
-          );
-        }
-      } else {
-        php_email_form_submit(thisForm, action, formData);
-      }
+      // if (recaptcha) {
+      //   if (typeof grecaptcha !== "undefined") {
+      //     grecaptcha.ready(function () {
+      //       try {
+      //         grecaptcha
+      //           .execute(recaptcha, { action: "php_email_form_submit" })
+      //           .then((token) => {
+      //             formData.set("recaptcha-response", token);
+      //             php_email_form_submit(thisForm, action, formData);
+      //           });
+      //       } catch (error) {
+      //         displayError(thisForm, error);
+      //       }
+      //     });
+      //   } else {
+      //     displayError(
+      //       thisForm,
+      //       "The reCaptcha javascript API url is not loaded!"
+      //     );
+      //   }
+      // } else {
+      //   php_email_form_submit(thisForm, action, formData);
+      // }
     });
   });
 
@@ -86,7 +86,7 @@
         displayError(thisForm, error);
       });
   }
-
+  php_email_form_submit(thisForm, action, formData);
   function displayError(thisForm, error) {
     thisForm.querySelector(".loading").classList.remove("d-block");
     thisForm.querySelector(".error-message").innerHTML = error;
