6aca80f115aa2430a0d0e42da69ad69f49a213f7
JSD1982 <cardoso@josesegundo.digital>
2024-01-05 15:39:01 -0300

    valid email

--- forms/contact.php
+++ forms/contact.php
@@ -5,13 +5,15 @@
   * The library should be uploaded to: vendor/php-email-form/php-email-form.php
   * For more info and help: https://bootstrapmade.com/php-email-form/
   */
- echo '<p>Hola Mundo</p>';
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'pachajsc@gmail.com';
 
-  
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
-
+  
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
